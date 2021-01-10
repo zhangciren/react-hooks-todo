8c58ebacbcbcc58ebaad2a8b004f6499c7d4c13e
zhangciren <zhangciren@meituan.com>
2021-01-10 14:39:17 +0800

    使用useReducer代替useState

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,17 +1,25 @@
-import React, { FC, ReactElement, useCallback, useState, useEffect } from 'react'
+import React, { FC, ReactElement, useCallback, useState, useEffect, useReducer } from 'react'
 import TdInput from './Input'
 import TdList from './List'
-import { ITodo } from './typings'
+import { ITodo, IState, ACTION_TYPE } from './typings'
+import { todoReducer } from './reducer'
 
+const initialState:IState = {
+  todoList: []
+}
 
 const TodoList: FC = (): ReactElement => {
 
-  const [todoList, setTodoList] = useState<ITodo[]>([])
+//   const [todoList, setTodoList] = useState<ITodo[]>([])
+  // 使用useReducer来代替useState：
+  // 比如这个todoList，你可能去add或者remove或者splice等等，也就是你有很多的方法去操作一个状态的变化，而且方法内部有比较相对复杂的逻辑时，就可以使用useReduce
+  // 如果你熟悉redux的话，就知道它是如何工作的。
+  const [state, dispatch] = useReducer(todoReducer, initialState) // 返回state和dispatch函数
 
 
   useEffect(() => {
-    console.log(todoList)
-  }, [todoList]) // 当todoList有改变的时候，打印todoList
+    console.log(state.todoList)
+  }, [state.todoList]) // 当todoList有改变的时候，打印todoList
 
   // 当父组件更新，子组件并没有更新的时候，这个句柄会重新生成一次。因此会有性能问题
   /*
@@ -21,14 +29,18 @@ const TodoList: FC = (): ReactElement => {
   */
   // 因此，要使用useCallback包裹
   const addTodo = useCallback((todo: ITodo) => {
-    setTodoList(todoList => [...todoList, todo])
+    // setTodoList(todoList => [...todoList, todo])
+    dispatch({
+      type: ACTION_TYPE.ADD_TODO,
+      payload: todo
+    })
   }, [])
 
   return (
     <div className="todo-list">
       <TdInput
         addTodo={ addTodo }
-        todoList={ todoList }
+        todoList={ state.todoList }
       />
       <TdList />
     </div>
---
+++ src/components/TodoList/reducer.ts
@@ -0,0 +1,38 @@
+import { IState, IAction, ACTION_TYPE, ITodo } from './typings'
+
+function todoReducer(state: IState, action: IAction):IState {
+    const { type, payload } = action
+
+    switch(type){
+      case ACTION_TYPE.ADD_TODO: 
+        return {
+          ...state,
+          todoList: [...state.todoList, payload as ITodo], // payload可能是ITodo，也可能是number。所以这里要做断言
+        }
+      case ACTION_TYPE.REMOVE_TODO:
+        return {
+          ...state,
+          todoList: state.todoList.filter(todo => todo.id !== payload)
+        }
+      case ACTION_TYPE.TOGGLE_TODO:
+        return {
+          ...state,
+          todoList: state.todoList.map(todo => {
+            return todo.id === payload ?
+            {
+              ...todo, 
+              completed: !todo.completed
+            } :
+            {
+              ...todo
+            }
+          })
+        }
+      default: 
+        return state
+    }
+}
+
+export {
+  todoReducer
+}
